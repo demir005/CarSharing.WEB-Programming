c1933789e2539a14e892bba04e032699e361e353
demir005 <demirmah93@gmail.com>
2017-05-05 16:48:26 +0200

    Login i Registracija
    Login jos malo ima da se uredi, neke funkcije
    Ubacene funkcije za provjeru username i passworda u bazi podataka
    Da li postoji isti email i username

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -6,15 +6,16 @@ class Users extends CI_Controller {
 		$this->load->library('form_validation');
 	}    
 	
+	// register user
 	public function register(){
 		$data['title'] = 'Sign Up';
 		
 		$this->form_validation->set_rules('name', 'Name','required');
 		$this->form_validation->set_rules('surname', 'Surname','required');
 		$this->form_validation->set_rules('address', 'Address','required');
-		$this->form_validation->set_rules('email', 'Email','required');
+		$this->form_validation->set_rules('email', 'Email','required|callback_check_email_exists');
 		$this->form_validation->set_rules('phoneNumber', 'Phone Number','required');
-		$this->form_validation->set_rules('username', 'Username','required');
+		$this->form_validation->set_rules('username', 'Username','required|callback_check_username_exists');
 		$this->form_validation->set_rules('password', 'Password','required');
 		$this->form_validation->set_rules('password2', 'Confirm Password','matches[password]');
 		
@@ -33,6 +34,101 @@ class Users extends CI_Controller {
 					'You are now registered, you can sign in now');
 			redirect('posts');
 			
+		}
+		
+	}
+	
+	
+	// Login user
+	public function Login(){
+		$data['title'] = 'Sign In';
+		
+		$this->form_validation->set_rules('username', 'Username','required|callback_check_username_exists');
+		$this->form_validation->set_rules('password', 'Password','required');
+		$this->form_validation->set_rules('password2', 'Confirm Password','matches[password]');
+		
+		if($this->form_validation->run()==FALSE){
+			//	$this->load->view('template/header.php');
+			$this->load->view('users/login',$data);
+			//$this->load->view('template/footer.php');
+			
+		}else{
+			//Get Username
+			
+			$username = $this->input->post('username');
+			
+			//Get and encrypt password
+			
+			$password = md5($this->input->post('password'));
+			
+			//Login user
+			
+			
+			$user_id=$this->User_model->login($username,$password);
+			
+			if($user_id){
+				//Create a session
+				$user_data=array(
+					'user_id'=>$user_id,
+					'username'=>$username,
+					'logged_in'=>true	
+				);
+				$this->session->set_userdata($user_data);
+				
+				$this->session->set_flashdata('user_loggedin',
+						'You are now logged in');
+				redirect('posts');
+				
+			}
+			else{
+					//Set message
+					$this->session->set_flashdata('login_failed',
+							'Login is invalid');
+					redirect('user/login');
+					
+					}
+			}
+			
+		
+	}
+	
+	
+	//Logout user
+	public function logout(){
+		//Unser user data
+		$this->session->unset_userdata('logged_in');
+		$this->session->unset_userdata('user_id');
+		$this->session->unset_userdata('username');
+		
+		//set message
+		
+		$this->session->set_flashdata('user_loggedout',
+				'You are now logged out');
+		redirect('user/login');
+		
+			}
+	}
+	
+	//Check if username exist
+	function check_username_exists($username){
+		$this->form_validation->set_message('check_username_exists','That username alredy exsists.Please chose different one');
+		
+		if($this->User_model->check_username_exists($username)){
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	
+	//Check if email exist
+ function check_email_exists($email){
+		$this->form_validation->set_message('check_email_exists','That email alredy exsists.Please chose different one');
+		
+		if($this->User_model->check_email_exists($email)){
+			return true;
+		}else{
+			return false;
 		}
 		
 	}
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -22,4 +22,45 @@ class User_model extends CI_Model{
 		
 		
 	}
+	
+	public function login($username,$password){
+		//Validate
+		
+		$this->db->where('username',$username);
+		$this->db->where('password',$password);
+		
+		$result = $this->db->get('users');
+		
+		
+		if($result->num_rows()==1){
+			return $result->row(0)->id;
+		}else{
+			return false;
+		}
+	}
+	
+	
+	
+	
+	//Check username exists
+	
+	public function check_username_exists($username){
+		$query = $this->db->get_where('users',array('username'=>$username));
+		if(empty($query->row_array())){
+			return true;
+		}
+		else{
+			return  false;
+		}
+	}
+	
+	public function check_email_exists($email){
+		$query = $this->db->get_where('users',array('email'=>$email));
+		if(empty($query->row_array())){
+			return true;
+		}
+		else{
+			return  false;
+		}
+	}
 }
